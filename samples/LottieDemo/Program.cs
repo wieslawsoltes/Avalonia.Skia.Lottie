570bf52077947730645609d42444b7ce12fee44c
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-06-09 14:09:59 +0200

    Update Program.cs

--- samples/LottieDemo/Program.cs
+++ samples/LottieDemo/Program.cs
@@ -13,5 +13,25 @@ class Program
         => AppBuilder.Configure<App>()
             .UsePlatformDetect()
             .UseSkia()
+            .With(new Win32PlatformOptions()
+            {
+                AllowEglInitialization = true,
+                UseWindowsUIComposition = true,
+                UseDeferredRendering = true
+            })
+            .With(new X11PlatformOptions()
+            {
+                UseGpu = true,
+                UseDeferredRendering = true
+            })
+            .With(new AvaloniaNativePlatformOptions()
+            {
+                UseGpu = true,
+                UseDeferredRendering = true
+            })
+            .With(new MacOSPlatformOptions()
+            {
+                ShowInDock = true
+            })
             .LogToTrace();
 }
