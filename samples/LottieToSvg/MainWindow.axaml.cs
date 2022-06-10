7472b0d8badc2cbd0f492da1f76f1b5cb7fffa20
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-06-10 21:42:31 +0200

    Add LottieToSvg

---
+++ samples/LottieToSvg/MainWindow.axaml.cs
@@ -0,0 +1,160 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Media.Imaging;
+using SkiaSharp;
+
+namespace LottieToSvg;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+#if DEBUG
+        this.AttachDevTools();
+#endif
+        AddHandler(DragDrop.DragOverEvent, DragOver);
+        AddHandler(DragDrop.DropEvent, Drop);
+    }
+
+    private void DragOver(object? sender, DragEventArgs e)
+    {
+        e.DragEffects &= DragDropEffects.Copy | DragDropEffects.Link;
+
+        if (!e.Data.Contains(DataFormats.FileNames))
+        {
+            e.DragEffects = DragDropEffects.None;
+        }
+    }
+
+    private async void Drop(object? sender, DragEventArgs e)
+    {
+        if (!e.Data.Contains(DataFormats.FileNames))
+        {
+            return;
+        }
+
+        var paths = e.Data.GetFileNames()?.ToList();
+        if (paths is null || paths.Count <= 0)
+        {
+            return;
+        }
+
+        await Task.Run(() =>
+        {
+            foreach (var path in paths)
+            {
+                ToSvg(path, "c:\\Temp\\");
+            }
+        });
+    }
+
+    // private List<Bitmap> _bs = new List<Bitmap>();
+
+    private void CreatePdf()
+    {
+        
+    }
+    
+    private void CreateXps()
+    {
+        
+    }
+
+    
+    private void ToSvg(string inputFilePath, string outputPath)
+    {
+        using var inputStream = File.OpenRead(inputFilePath);
+        using var inputManagedStream = new SKManagedStream(inputStream);
+
+        if (!SkiaSharp.Skottie.Animation.TryCreate(inputManagedStream, out var animation))
+        {
+            return;
+        }
+
+        var inPoint = animation.InPoint;
+        var outPoint = animation.OutPoint;
+        var dst = new SKRect(0, 0, animation.Size.Width, animation.Size.Height);
+
+
+
+
+
+        {
+            var outputFileName = $"{Path.GetFileNameWithoutExtension(inputFilePath)}.pdf";
+            var outputFilePath = Path.Combine(outputPath, outputFileName);
+            using var outputStream = new SKFileWStream(outputFilePath);
+            using var document = SKDocument.CreatePdf(outputStream, SKDocument.DefaultRasterDpi);
+
+            for (var t = inPoint; t <= outPoint; t += 1d)
+            {
+                using var canvas = document.BeginPage(animation.Size.Width, animation.Size.Height);
+                animation.SeekFrame(t);
+                animation.Render(canvas, dst);
+            }
+
+            document.Close();
+        }
+
+        {
+            var outputFileName = $"{Path.GetFileNameWithoutExtension(inputFilePath)}.xps";
+            var outputFilePath = Path.Combine(outputPath, outputFileName);
+            using var outputStream = new SKFileWStream(outputFilePath);
+            using var document = SKDocument.CreateXps(outputStream, SKDocument.DefaultRasterDpi);
+
+            for (var t = inPoint; t <= outPoint; t += 1d)
+            {
+                using var canvas = document.BeginPage(animation.Size.Width, animation.Size.Height);
+                animation.SeekFrame(t);
+                animation.Render(canvas, dst);
+            }
+
+            document.Close();
+        }
+
+
+
+        for (var t = inPoint; t <= outPoint; t += 1d)
+        {
+            // var outputFileExtension = "svg";
+            //var outputFileExtension = "png";
+            //var outputFileName = $"{Path.GetFileNameWithoutExtension(inputFilePath)}.{t:0000}.{outputFileExtension}";
+            //var outputFilePath = Path.Combine(outputPath, outputFileName);
+
+            /*
+            using var outputStream = File.Create(outputFilePath);
+            using var outputManagedStream = new SKManagedWStream(outputStream);
+
+            var bounds = SKRect.Create(animation.Size);
+            using var canvas = SKSvgCanvas.Create(bounds, outputManagedStream);
+
+            animation.SeekFrame(t);
+            animation.Render(canvas, dst);
+            //*/
+
+            /*
+            var imageInfo = new SKImageInfo((int)animation.Size.Width, (int)animation.Size.Height, SKImageInfo.PlatformColorType, SKAlphaType.Unpremul);
+            using var bitmap = new SKBitmap(imageInfo);
+            using var canvas = new SKCanvas(bitmap);
+
+            animation.SeekFrame(t);
+            animation.Render(canvas, dst);
+
+            using var image = SKImage.FromBitmap(bitmap);
+            using var data = image.Encode(SKEncodedImageFormat.Png, 100);
+
+            using var outputStream = File.Create(outputFilePath);
+            data.SaveTo(outputStream);
+
+            // var b = new Bitmap(data.AsStream());
+            // _bs.Add(b);
+
+            //*/
+        }
+    }
+}
